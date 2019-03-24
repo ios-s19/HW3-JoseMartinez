3eda8dafc9defebed856528c08de7cd805b39c19
marti265 <marti265@rangers.uwp.edu>
2019-03-23 19:15:54 -0500

    Initial Commit

---
+++ HW3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW3
+//
+//  Created by Jose Martinez on 3/23/19.
+//  Copyright © 2019 Jose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
